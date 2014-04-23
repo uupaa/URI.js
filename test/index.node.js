77aedb088d6a663a797c7d826c93d4f1911160b4
uupaa <uupaa.js@gmail.com>
2014-04-23 15:50:41 +0900

    add build setting

--- test/index.node.js
+++ test/index.node.js
@@ -1,7 +1,6 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../lib/URI.js");
 require("../release/URI.min.js");
 require("./test.js");
-
